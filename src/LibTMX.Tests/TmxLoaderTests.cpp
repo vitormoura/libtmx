f027b66ac5f421f3f939c80ded8eb0dc9ee7d53f
Vitor Moura <indrema.dev@gmail.com>
2017-09-26 14:54:51 -0300

    complementacao testes leitura do arquivo tmx completo

--- src/LibTMX.Tests/TmxLoaderTests.cpp
+++ src/LibTMX.Tests/TmxLoaderTests.cpp
@@ -44,7 +44,7 @@ TEST(TmxLoaderTests, CargaArquivoValidoRetornaTrue) {
 
 	//Tileset
 	ASSERT_TRUE(map.tilesets.size() == 1);
-
+		
 	auto ts = map.tilesets[0];
 
 	ASSERT_EQ(ts->firstgid, 1);
@@ -55,15 +55,54 @@ TEST(TmxLoaderTests, CargaArquivoValidoRetornaTrue) {
 	ASSERT_EQ(ts->columns, 8);
 	ASSERT_EQ(ts->tile_offset.x, 15);
 	ASSERT_EQ(ts->tile_offset.y, 15);
-
+	
+	//Tileset/image
+	
 	ASSERT_EQ(ts->image->format, "");
 	ASSERT_EQ(ts->image->width, 256);
 	ASSERT_EQ(ts->image->height, 7199);
 	ASSERT_STREQ(ts->image->transparency_color.c_str(), "FF00FF");
 	ASSERT_EQ(ts->image->source, "lab_tilemap_001.png");
+	ASSERT_TRUE(ts->image->data == nullptr);
 
+	//Tileset/tiles
+
+	ASSERT_TRUE(ts->tiles != nullptr);
 	ASSERT_TRUE(ts->tiles->size() == 3);
-		
+
+	auto ts_0 = ts->tiles->at(0);
+
+	ASSERT_EQ(ts_0->id, 44);
+	ASSERT_EQ(ts_0->properties->size(), 1);
+	ASSERT_EQ(ts_0->properties->at(0)->name, "Type");
+	ASSERT_EQ(ts_0->properties->at(0)->value, "teste");
+
+	ASSERT_TRUE(ts_0->object_group != nullptr);
+	ASSERT_EQ(ts_0->object_group->draw_order, object_group::draworder::index );
+	ASSERT_EQ(ts_0->object_group->name, "");
+	
+	ASSERT_TRUE(ts_0->object_group->properties != nullptr);
+	ASSERT_EQ(ts_0->object_group->properties->size(), 2);
+	ASSERT_EQ(ts_0->object_group->properties->at(0)->name, "Tag");
+	ASSERT_EQ(ts_0->object_group->properties->at(0)->value, "Pared");
+
+	ASSERT_TRUE(ts_0->object_group->objects != nullptr);
+	ASSERT_EQ(ts_0->object_group->objects->size(), 1);
+	ASSERT_EQ(ts_0->object_group->objects->at(0)->id, 5);
+	ASSERT_EQ(ts_0->object_group->objects->at(0)->x, 0);
+	ASSERT_EQ(ts_0->object_group->objects->at(0)->y, 0);
+	ASSERT_EQ(ts_0->object_group->objects->at(0)->width, 32);
+	ASSERT_EQ(ts_0->object_group->objects->at(0)->height, 32);
+
+	auto ts_2 = ts->tiles->at(2);
+
+	ASSERT_EQ(ts_2->object_group->objects->size(), 1);
+	ASSERT_EQ(ts_2->object_group->objects->at(0)->id, 1);
+	ASSERT_EQ(ts_2->object_group->objects->at(0)->x, -3);
+	ASSERT_EQ(ts_2->object_group->objects->at(0)->y, -2);
+	ASSERT_EQ(ts_2->object_group->objects->at(0)->width, 40);
+	ASSERT_EQ(ts_2->object_group->objects->at(0)->height, 37);
+
 	//TODO: implementar demais verificações
 
 	map.unload();
